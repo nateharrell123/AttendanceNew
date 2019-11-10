1d7c070a43e1b5d8e8a910808b0cbf17e341429e
nateharrell <nateharrell@CIS-RDS3>
2019-11-09 21:55:32 -0600

    Add project files.

---
+++ Attendance2/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Attendance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxDateText = new System.Windows.Forms.TextBox();
+            this.uxDate = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.uxFileStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxNewDate = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveResultsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxImportRoster = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.uxOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.uxRosterNameLabel = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // uxDateText
+            // 
+            this.uxDateText.Location = new System.Drawing.Point(78, 39);
+            this.uxDateText.Multiline = true;
+            this.uxDateText.Name = "uxDateText";
+            this.uxDateText.Size = new System.Drawing.Size(139, 32);
+            this.uxDateText.TabIndex = 0;
+            this.uxDateText.TextChanged += new System.EventHandler(this.UxDateText_TextChanged);
+            // 
+            // uxDate
+            // 
+            this.uxDate.AutoSize = true;
+            this.uxDate.Location = new System.Drawing.Point(105, 86);
+            this.uxDate.Name = "uxDate";
+            this.uxDate.Size = new System.Drawing.Size(0, 13);
+            this.uxDate.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Today\'s Date:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter Date:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uxFileStrip});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // uxFileStrip
+            // 
+            this.uxFileStrip.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uxNewDate,
+            this.saveResultsToolStripMenuItem,
+            this.uxImportRoster});
+            this.uxFileStrip.Name = "uxFileStrip";
+            this.uxFileStrip.Size = new System.Drawing.Size(37, 20);
+            this.uxFileStrip.Text = "File";
+            // 
+            // uxNewDate
+            // 
+            this.uxNewDate.Name = "uxNewDate";
+            this.uxNewDate.Size = new System.Drawing.Size(180, 22);
+            this.uxNewDate.Text = "Enter New Date";
+            this.uxNewDate.Click += new System.EventHandler(this.UxNewDate_Click);
+            // 
+            // saveResultsToolStripMenuItem
+            // 
+            this.saveResultsToolStripMenuItem.Name = "saveResultsToolStripMenuItem";
+            this.saveResultsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveResultsToolStripMenuItem.Text = "Save Results";
+            // 
+            // uxImportRoster
+            // 
+            this.uxImportRoster.Name = "uxImportRoster";
+            this.uxImportRoster.Size = new System.Drawing.Size(180, 22);
+            this.uxImportRoster.Text = "Import Roster";
+            this.uxImportRoster.Click += new System.EventHandler(this.UxImportRoster_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(515, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Roster in use:";
+            // 
+            // uxOpenFileDialog
+            // 
+            this.uxOpenFileDialog.FileName = "openFileDialog1";
+            // 
+            // uxRosterNameLabel
+            // 
+            this.uxRosterNameLabel.AutoSize = true;
+            this.uxRosterNameLabel.Location = new System.Drawing.Point(593, 39);
+            this.uxRosterNameLabel.Name = "uxRosterNameLabel";
+            this.uxRosterNameLabel.Size = new System.Drawing.Size(0, 13);
+            this.uxRosterNameLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.uxRosterNameLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uxDate);
+            this.Controls.Add(this.uxDateText);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox uxDateText;
+        private System.Windows.Forms.Label uxDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem uxFileStrip;
+        private System.Windows.Forms.ToolStripMenuItem uxNewDate;
+        private System.Windows.Forms.ToolStripMenuItem saveResultsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uxImportRoster;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog uxOpenFileDialog;
+        private System.Windows.Forms.Label uxRosterNameLabel;
+    }
+}
---
+++ Attendance2/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Attendance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string date; // final date
+        private void CheckEnterKeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                date = uxDateText.Text;
+                uxDateText.Enabled = false;
+                uxDate.Text = date;
+            }
+        }
+        private void UxDateText_TextChanged(object sender, EventArgs e)
+        {
+            this.uxDateText.KeyPress += new System.Windows.Forms.KeyPressEventHandler(CheckEnterKeyPress);
+        }
+
+        private void UxNewDate_Click(object sender, EventArgs e)
+        {
+            uxDateText.Enabled = true;
+            date = uxDateText.Text;
+            uxDate.Text = date;
+        }
+
+        private void UxImportRoster_Click(object sender, EventArgs e)
+        {
+            if (uxOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = uxOpenFileDialog.FileName;
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    uxRosterNameLabel.Text = fileName;
+                }
+            }
+        }
+    }
+}
