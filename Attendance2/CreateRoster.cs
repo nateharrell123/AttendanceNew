eba00c01eefe61d26799f01e655a53db5b0331e8
nateharrell <nateharrell@CIS-RDS4>
2020-01-08 16:41:24 -0600

    pre - boolean checks (10 IQ)

--- Attendance2/CreateRoster.cs
+++ Attendance2/CreateRoster.cs
@@ -26,8 +26,6 @@ namespace Attendance2
 
         Attendance3 attendance3 = new Attendance3();
 
-        bool rosterEdited = false;
-
         bool isFinalized = false;
 
 
@@ -40,19 +38,20 @@ namespace Attendance2
         {
             try
             {
+               
                 if (names.Count > 0)
                 {
                     string removed = names[names.Count - 1];
                     nameDisplay.Clear();
-                    foreach(string name in names)
+                    foreach (string name in names)
                     {
                         nameDisplay.Add(name);
                     }
                     names.Remove(names[names.Count - 1]);
-                    
+
                     uxRemovedText.Text = "Removed " + removed + " from the roster.";
                 }
-                else if(names.Count == 0)
+                else if (names.Count == 0)
                 {
                     throw new Exception();
                 }
@@ -72,7 +71,7 @@ namespace Attendance2
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void input_KeyDown(object sender, KeyPressEventArgs e)
-       {
+        {
             if (e.KeyChar == (char)Keys.Enter)
             {
                 string promptText = uxNamesTextBox.Text;
@@ -85,13 +84,12 @@ namespace Attendance2
                 else
                 {
                     names.Add(promptText.Trim());
-                    nameDisplay.Add(uxNamesTextBox.Text);
+                    nameDisplay.Add(promptText);
                     e.Handled = true;
                     uxNamesTextBox.Clear();
                     UpdateDisplay();
                 }
             }
-
         }
 
         /// <summary>
@@ -108,6 +106,7 @@ namespace Attendance2
                     {
                         displayName += name + "\r\n";
                     }
+
                 }
                 uxFileContentsTextBox.Text = displayName;
                 uxFilePreviewText.Font = new Font("Microsoft Sans Serif", 10);
@@ -129,25 +128,9 @@ namespace Attendance2
             else return false;
         }
 
-        private bool HasBeenEdited()
-        {
-            if (rosterEdited)
-            {
-                rosterEdited = false;
-                return rosterEdited;
-            }
-            return true;
-        }
 
-        private bool HasBeenFinalized()
-        {
-            if (isFinalized)
-            {
-                isFinalized = false;
-                return isFinalized;
-            }
-            else return true;
-        }
+
+
         /// <summary>
         /// Exports the roster as a .txt file.
         /// </summary>
@@ -157,16 +140,7 @@ namespace Attendance2
         {
             try
             {
-                //if(!HasBeenFinalized())
-                //{
-                //    MessageBox.Show("You need to finalize your roster before you can export.");
-                //    return;
-                //}
-                //if(HasBeenEdited())
-                //{
-                //    MessageBox.Show("You cannot export while editing a roster.");
-                //    return;
-                //}
+
                 if(names.Count == 0)
                 {
                     return;
@@ -235,7 +209,6 @@ namespace Attendance2
 
         private void EditRoster()
         {
-            rosterEdited = false;
             uxFileContentsTextBox.ReadOnly = false;
             uxNamesTextBox.ReadOnly = true;
             uxFilePreviewText.Text = "Press 'TAB' to submit your changes.";
@@ -245,7 +218,6 @@ namespace Attendance2
 
         private void DoneEditingRoster()
         {
-            rosterEdited = true;
             uxFileContentsTextBox.ReadOnly = true;
             uxNamesTextBox.ReadOnly = false;
             uxFilePreviewText.Text = "Successfully edited roster.";
@@ -259,16 +231,17 @@ namespace Attendance2
         /// <param name="e"></param>
         private void AdjustRosterToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if(IsEmpty() && isFinalized == false)
+            if(IsEmpty())
             {
                 return;
             }
 
 
-            if(isFinalized == false)
+            else
             {
                 EditRoster();
             }
+
         }
 
 
@@ -284,7 +257,7 @@ namespace Attendance2
                 string[] lines = uxFileContentsTextBox.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None); // Stack OverFlow
                 names.Clear();
                 
-                for(int i = 0; i < lines.Length; i++)
+                for(int i = 0; i < lines.Length - 1; i++) 
                 {
                     names.Add(lines[i]);
                 }
@@ -299,7 +272,7 @@ namespace Attendance2
 
         private void ClearRosterToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (names.Count > 0 && isFinalized == false)
+            if (names.Count > 0)
             {
                 uxFileContentsTextBox.Text = string.Empty;
                 uxRemovedText.Text = "";
